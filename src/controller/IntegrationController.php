ae118db8b04b7c7941d66de4a68e758849da57f3
Mikael Lindqvist <li.mikael@gmail.com>
2017-03-02 11:33:51 +0300

    added integration model and controller

---
+++ src/controller/IntegrationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace kpiint;
+
+/**
+ * Controller for integrations.
+ */
+class IntegrationController extends Singleton {
+
+	/**
+	 * Constructor.
+	 */
+	protected function __construct() {
+
+	}
+}
---
+++ src/model/Integration.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace kpiint;
+
+/**
+ * Represents one integration to an external site.
+ */
+class Integration extends PostTypeModel {
+
+}
